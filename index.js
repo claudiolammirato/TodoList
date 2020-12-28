623b58cf6f2303d3028a3cbbad8c129deb33f451
claudiolammirato <claudio.lammirato@gmail.com>
2020-12-28 17:58:36 +0100

    init

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-aaa
+const express = require('express');
+const app = express();
+
+const todosRoutes = require('./routes/todos');
+
+
+//app.use('/todos/:id', logger);
+
+app.use('/todos', todosRoutes);
+
+app.listen(4000, ()=> console.log('Listening on port 4000'));
---
+++ routes/todos.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const logger = (req, resp, next) =>{
+    if(req.params.id>100){
+        next(new Error('id cannot be grater than 100'));
+    }
+    next();
+}
+
+router.all('*',(req, resp, next)=>{
+    console.log('I am the all middleware');
+    next();
+})
+
+router.get('/',(req,res)=>{
+    res.send('todos');
+})
+
+router.get('/:id([0-9]+)', [logger, (req,res)=>{
+    res.send('todo with id ' + req.params.id);
+}])
+
+module.exports = router;
